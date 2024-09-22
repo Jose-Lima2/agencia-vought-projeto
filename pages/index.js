1b2a5be5f426affea68bb945af225f10c2cc71da
Jose-Lima2 <joselima290184@gmail.com>
2024-09-22 06:28:01 -0300

    cores

--- pages/index.js
+++ pages/index.js
@@ -14,10 +14,10 @@ import { fadeIn } from '../variants';
 
 const Home = () => {
   return (
-    <div className='bg-primary/60 h-full'>
+    <div className='h-full bg-primary/60'>
       {/* text */}
       <div className='w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10'>
-        <div className='text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto'>
+        <div className='container flex flex-col justify-center h-full mx-auto text-center xl:pt-40 xl:text-left'>
           {/* title */}
           <motion.h1
             variants={fadeIn('down', 0.2)}
@@ -35,12 +35,12 @@ const Home = () => {
             initial='hidden'
             animate='show'
             exit='hidden'
-            className='max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16'
+            className='max-w-sm mx-auto mb-10 xl:max-w-xl xl:mx-0 xl:mb-16'
           >
             A Vought oferece produção de treinamentos online e gestão de redes sociais para maximizar sua presença digital de forma estratégica e inovadora.
           </motion.p>
           {/* btn */}
-          <div className='flex justify-center xl:hidden relative'>
+          <div className='relative flex justify-center xl:hidden'>
             <ProjectsBtn />
           </div>
           <motion.div
@@ -57,7 +57,7 @@ const Home = () => {
       {/* image */}
       <div className='w-[1200px] h-full absolute right-0 bottom-0'>
         {/* bg img */}
-        <div className='bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0'></div>
+        <div className='absolute w-full h-full bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat mix-blend-color-dodge translate-z-0'></div>
         {/* particles */}
         <ParticlesContainer />
         {/* avatar img */}
