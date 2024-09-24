0b0e49844a7f0a2122eae27629dc2e2244132daf
Jose-Lima2 <joselima290184@gmail.com>
2024-09-24 18:27:42 -0300

    tempo

--- components/ParticlesContainer.js
+++ components/ParticlesContainer.js
@@ -12,7 +12,7 @@ const ParticlesContainer = () => {
 
   return (
     <Particles
-      className='w-full h-full absolute translate-z-0'
+      className='absolute w-full h-full translate-z-0'
       id='tsparticles'
       init={particlesInit}
       loaded={particlesLoaded}
@@ -42,7 +42,7 @@ const ParticlesContainer = () => {
             },
             repulse: {
               distance: 200,
-              duration: 0.4,
+              duration: 0.6,
             },
           },
         },
